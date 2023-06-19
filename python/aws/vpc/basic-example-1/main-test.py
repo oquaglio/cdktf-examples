dc8243014c1ef85102d8dcdd444d7c7bbdc6dc46
Otto Quaglio <otto.q@gmx.com>
2023-06-19 09:47:54 +0800

    add experimentation code

--- python/aws/vpc/basic-example-1/main-test.py
+++ python/aws/vpc/basic-example-1/main-test.py
@@ -1,5 +1,7 @@
+import inspect
 import pytest
 from cdktf import Testing
+from cdktf import App, TerraformStack
 
 # The tests below are example tests, you can find more information at
 # https://cdk.tf/testing
@@ -10,9 +12,29 @@ class TestMain:
     def test_my_app(self):
         assert True
 
-    #stack = TerraformStack(Testing.app(), "stack")
+    stack = TerraformStack(Testing.app(), "stack")
     #app_abstraction = MyApplicationsAbstraction(stack, "app-abstraction")
-    #synthesized = Testing.synth(stack)
+    synthesized = Testing.synth(stack)
+    print(synthesized)
+    print(type(synthesized))
+    print(type(stack))
+    print(dir(synthesized))
+    print(dir(stack))
+    print(stack.to_string())
+    print(stack.depends_on)
+    print(stack.dependencies)
+    print(getattr(stack, "depends_on"))
+
+
+    # Print information about the class's constructor
+    sig = inspect.signature(TerraformStack.__init__)
+    for param in sig.parameters.values():
+        print(f"{param.name}: {param.default}")
+
+    # Print information about the class's methods
+    for name, member in inspect.getmembers(TerraformStack, inspect.ismethod):
+        if name != "__init__":
+            print(f"{name}: {member.__doc__}")
 
     # def test_should_contain_container(self):
     #    assert Testing.to_have_resource(self.synthesized, Container.TF_RESOURCE_TYPE)
@@ -22,5 +44,5 @@ class TestMain:
     #        "name": "ubuntu:latest",
     #    })
 
-    # def test_check_validity(self):
-    #    assert Testing.to_be_valid_terraform(Testing.full_synth(stack))
+    def test_check_validity(self):
+       assert Testing.to_be_valid_terraform(Testing.full_synth(stack))
